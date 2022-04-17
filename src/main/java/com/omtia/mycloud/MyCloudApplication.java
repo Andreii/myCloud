14ffe932ba3fdb9b596e5e683dd7d0f31d8f81fb
act <act@mediamid.com>
2022-04-17 20:39:14 +0200

    oauth2 user

--- src/main/java/com/omtia/mycloud/MyCloudApplication.java
+++ src/main/java/com/omtia/mycloud/MyCloudApplication.java
@@ -2,6 +2,9 @@ package com.omtia.mycloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class MyCloudApplication {
@@ -10,4 +13,8 @@ public class MyCloudApplication {
         SpringApplication.run(MyCloudApplication.class, args);
     }
 
+    @RequestMapping("/user")
+    public String user(@AuthenticationPrincipal OAuth2User principal) {
+        return principal.getAttribute("name");
+    }
 }
